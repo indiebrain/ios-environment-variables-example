7f65604ab452c3ea0ea5e62639c8658dc4813db1
Aaron Kuehler <aaron.kuehler@gmail.com>
2014-07-13 13:29:38 -0400

    Initial Commit

---
+++ MyAppIOS/MAIOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MAIOSAppDelegate.h
+//  MyAppIOS
+//
+//  Created by Aaron Kuehler on 7/13/14.
+//  Copyright (c) 2014 Aaron Kuehler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAIOSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyAppIOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyAppIOS
+//
+//  Created by Aaron Kuehler on 7/13/14.
+//  Copyright (c) 2014 Aaron Kuehler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MAIOSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MAIOSAppDelegate class]));
+    }
+}
